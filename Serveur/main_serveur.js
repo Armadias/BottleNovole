210f85dfcda893c938b7b5beb8524d8462dfd001
Armadia <follietmartin@gmail.com>
2022-01-03 10:43:31 +0100

    mysql insert

--- Serveur/main_serveur.js
+++ Serveur/main_serveur.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const mysql = require('mysql'); 
 const app = express()
 const port = 3000
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.dtb_password
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 app.set("view engine", "ejs")
 
